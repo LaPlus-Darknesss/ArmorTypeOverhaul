e83ec3fc3617329c27729be543d934eeb15a623b
LaPlus-Darknesss <cyuriiosity@gmail.com>
2025-10-28 02:58:41 -0700

    feat(light): add scaffolding for Lone Wolf + Shadowstep

--- Scripts/ATO_Init.lua
+++ Scripts/ATO_Init.lua
@@ -1,4 +1,6 @@
 -- Scripts/ATO_Init.lua
+-- Initializes Armor Type Overhaul (ATO). Hooks basic events and loads helpers.
+
 Ext.Require("Scripts/ATO_Events.lua")
 
 Ext.Events.SessionLoaded:Subscribe(function (_)
